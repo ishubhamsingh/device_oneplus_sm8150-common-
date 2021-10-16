4296de834d5348a400ad8f670b794889ba5c79d0
Shubham Singh <coolsks94@gmail.com>
2021-10-16 11:09:43 +0000

    {DNM] sm8916-common: Bypass build checks

--- common.mk
+++ common.mk
@@ -12,6 +12,9 @@ $(call inherit-product, vendor/oneplus/sm8150-common/sm8150-common-vendor.mk)
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+RELAX_USES_LIBRARY_CHECK := true
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
