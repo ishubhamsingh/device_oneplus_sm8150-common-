f006a888e9efac0946e7bff8e0b8ea824cc32758
TheScarastic <warabhishek@gmail.com>
2021-10-19 20:48:39 +0000

    sm8150-common: Introduce biometric 2.3 hal
     * wrap 2.1 hal and add additional function
     * Remove lineage fod hal
     * Rename fodextenssion to udfpsextension
    
      @ishubhamsingh - Adapted for Oneplus sm8150
    
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

---
+++ biometrics/BiometricsFingerprint.cpp
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.oneplus_msmnile"
+
+#include "BiometricsFingerprint.h"
+#include <hidl/HidlTransportSupport.h>
+#include <android-base/logging.h>
+#include <fstream>
+
+#define OP_ENABLE_FP_LONGPRESS 3
+#define OP_DISABLE_FP_LONGPRESS 4
+#define OP_RESUME_FP_ENROLL 8
+#define OP_FINISH_FP_ENROLL 10
+
+#define OP_DISPLAY_AOD_MODE 8
+#define OP_DISPLAY_NOTIFY_PRESS 9
+#define OP_DISPLAY_SET_DIM 10
+
+namespace android {
+namespace hardware {
+namespace biometrics {
+namespace fingerprint {
+namespace V2_3 {
+namespace implementation {
+
+BiometricsFingerprint::BiometricsFingerprint() {
+    biometrics_2_1_service = IBiometricsFingerprint_2_1::getService();
+    mVendorFpService = IVendorFingerprintExtensions::getService();
+    mVendorDisplayService = IOneplusDisplay::getService();
+}
+
+Return<uint64_t> BiometricsFingerprint::setNotify(const sp<IBiometricsFingerprintClientCallback>& clientCallback) {
+    return biometrics_2_1_service->setNotify(clientCallback);
+}
+
+Return<uint64_t> BiometricsFingerprint::preEnroll() {
+    return biometrics_2_1_service->preEnroll();
+}
+
+Return<RequestStatus> BiometricsFingerprint::enroll(const hidl_array<uint8_t, 69>& hat, uint32_t gid, uint32_t timeoutSec) {
+    mVendorFpService->updateStatus(OP_DISABLE_FP_LONGPRESS);
+    mVendorFpService->updateStatus(OP_RESUME_FP_ENROLL);
+    return biometrics_2_1_service->enroll(hat, gid, timeoutSec);
+}
+
+Return<RequestStatus> BiometricsFingerprint::postEnroll() {
+    return biometrics_2_1_service->postEnroll();
+}
+
+Return<uint64_t> BiometricsFingerprint::getAuthenticatorId() {
+    return biometrics_2_1_service->getAuthenticatorId();
+}
+
+Return<RequestStatus> BiometricsFingerprint::cancel() {
+    mVendorFpService->updateStatus(OP_FINISH_FP_ENROLL);
+    return biometrics_2_1_service->cancel();
+}
+
+Return<RequestStatus> BiometricsFingerprint::enumerate() {
+    return biometrics_2_1_service->enumerate();
+}
+
+Return<RequestStatus> BiometricsFingerprint::remove(uint32_t gid, uint32_t fid) {
+    return biometrics_2_1_service->remove(gid, fid);
+}
+
+Return<RequestStatus> BiometricsFingerprint::setActiveGroup(uint32_t gid, const hidl_string& storePath) {
+    return biometrics_2_1_service->setActiveGroup(gid, storePath);
+}
+
+Return<RequestStatus> BiometricsFingerprint::authenticate(uint64_t operationId, uint32_t gid) {
+    mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
+    return biometrics_2_1_service->authenticate(operationId, gid);
+}
+
+Return<bool> BiometricsFingerprint::isUdfps(uint32_t) {
+    return true;
+}
+
+Return<void> BiometricsFingerprint::onFingerDown(uint32_t, uint32_t, float, float) {
+    mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 1);
+    mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 1);
+    return Void();
+}
+
+Return<void> BiometricsFingerprint::onFingerUp() {
+    mVendorDisplayService->setMode(OP_DISPLAY_SET_DIM, 0);
+    mVendorDisplayService->setMode(OP_DISPLAY_NOTIFY_PRESS, 0);
+    return Void();
+}
+
+}  // namespace implementation
+}  // namespace V2_3
+}  // namespace fingerprint
+}  // namespace biometrics
+}  // namespace hardware
+}  // namespace android
